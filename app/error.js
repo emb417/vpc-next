8c07cce47efb16fd255777acfc601772efb1d894
Eric Brousseau <emb417@gmail.com>
2024-12-29 15:02:23 -0800

    formatted error page

--- app/error.js
+++ app/error.js
@@ -8,15 +8,15 @@ export default function Error({ error, reset }) {
   }, [error]);
 
   return (
-    <div className="flex flex-col h-screen mb-40 justify-center items-center">
-      <h2 className="flex justify-center items-center text-3xl text-orange-600">
+    <div className="flex flex-col h-dvh w-full mt-10 items-center">
+      <h2 className="text-3xl text-orange-600">
         ZZzzzZZZzzZzzZZZ!
       </h2>
       <button
         className="border-2 border-orange-600 rounded-full px-4 py-2 mt-4 text-orange-600 hover:bg-orange-600 hover:text-stone-50"
         onClick={() => reset()}
       >
-        Try again
+        Reload
       </button>
     </div>
   );
