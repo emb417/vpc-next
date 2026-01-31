75f62f80f82bc30e2394a7e70bf8ec703d8d3788
Eric Brousseau <emb417@gmail.com>
2026-01-31 11:21:59 -0800

    moved client side security headers from nginx to nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,72 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: `
+      default-src 'self';
+      img-src 'self' data: https://cdn.discordapp.com https://virtualpinballspreadsheet.github.io;
+      connect-src 'self' cloudflareinsights.com;
+      style-src 'self' 'unsafe-inline';
+      script-src 'self' 'unsafe-inline' static.cloudflareinsights.com;
+      form-action 'self';
+      frame-src 'none';
+      frame-ancestors 'self';
+      object-src 'none';
+      base-uri 'none';
+    `
+      .replace(/\s{2,}/g, " ")
+      .trim(),
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Permissions-Policy",
+    value:
+      "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()",
+  },
+];
+
 const nextConfig = {
   output: "standalone",
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.discordapp.com",
-        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "virtualpinballspreadsheet.github.io",
-        port: "",
         pathname: "/**",
       },
     ],
   },
+
   env: {
     APP_VERSION: process.env.npm_package_version,
     METADATA_URL: "https://virtualpinballchat.com:8443",
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
