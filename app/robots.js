e48acdc0db211c8746f4756a45846b932fdab5cd
Eric Brousseau <emb417@gmail.com>
2026-02-01 11:56:03 -0800

    updated robots and sitemap to remove 8443

--- app/robots.js
+++ app/robots.js
@@ -1,9 +1,9 @@
 export default function robots() {
-    return {
-      rules: {
-        userAgent: '*',
-        allow: '/',
-      },
-      sitemap: 'https://virtualpinballchat.com:8443/sitemap.xml',
-    }
-  }
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://virtualpinballchat.com/sitemap.xml",
+  };
+}
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,28 +1,34 @@
 export default function sitemap() {
-    return [
-      {
-        url: 'https://virtualpinballchat.com:8443',
-        lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 1,
-      },
-      {
-        url: 'https://virtualpinballchat.com:8443/season',
-        lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://virtualpinballchat.com:8443/history',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-      {
-        url: 'https://virtualpinballchat.com:8443/stats',
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-        priority: 0.3,
-      },
-    ]
-  }
+  return [
+    {
+      url: "https://virtualpinballchat.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://virtualpinballchat.com/season",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://virtualpinballchat.com/competition",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.5,
+    },
+    {
+      url: "https://virtualpinballchat.com/highscores",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.5,
+    },
+    {
+      url: "https://virtualpinballchat.com/stats",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.3,
+    },
+  ];
+}
