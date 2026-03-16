1b45e87d508dac706f8ad6cd216843a16be2dbc0
Eric Brousseau <emb417@gmail.com>
2026-03-16 08:35:09 -0700

    added text to toggle

--- components/nav/ThemeToggle.js
+++ components/nav/ThemeToggle.js
@@ -9,12 +9,18 @@ export default function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 rounded-md hover:bg-stone-300 dark:hover:bg-stone-700 transition-colors"
+      className="flex flex-row items-center gap-1 px-2 py-1 rounded-md hover:bg-stone-300 dark:hover:bg-stone-700 transition-colors"
     >
       {theme === "light" ? (
-        <GiMoon className="text-orange-600 text-xl" />
+        <>
+          <GiMoon className="text-orange-600 text-xl" />
+          <span>Dark</span>
+        </>
       ) : (
-        <GiSun className="text-orange-400 text-xl" />
+        <>
+          <GiSun className="text-orange-400 text-xl" />
+          <span>Light</span>
+        </>
       )}
     </button>
   );
