a7b4960744d875c2c49daedf043b5399ababc32f
Eric Brousseau <emb417@gmail.com>
2026-03-17 10:26:10 -0700

    fix for white flash when loading dark mode

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,10 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{
             __html: `
               (function() {
-                const t = localStorage.getItem('theme');
-                if (t === 'dark') document.documentElement.classList.add('dark');
+                const theme = localStorage.getItem('theme');
+                if (theme === 'dark') document.documentElement.classList.add('dark');
+                else if (theme === 'light') document.documentElement.classList.remove('dark');
+                else if (window.matchMedia('(prefers-color-scheme: dark)').matches) document.documentElement.classList.add('dark');
               })();
             `,
           }}
