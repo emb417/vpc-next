4eb82c924ae0094d287367f4b90b49fbe00e8d71
Eric Brousseau <emb417@gmail.com>
2026-07-11 23:45:44 -0700

    fix for dark theme hydration error in browser

--- components/nav/ThemeToggle.js
+++ components/nav/ThemeToggle.js
@@ -2,9 +2,19 @@
 
 import { useTheme } from "@/lib/ThemeContext";
 import { GiMoon, GiSun } from "react-icons/gi";
+import { useState, useEffect } from "react";
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <button
